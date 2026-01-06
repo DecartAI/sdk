deb38c0c2ef31f688824241aca81c731fd294f07
Guy Tepper <hey@guytepper.com>
2026-01-06 10:15:58 +0100

    remove next/express index export
    need to import them directly now

--- packages/proxy/src/index.ts
+++ packages/proxy/src/index.ts
@@ -1,10 +1,2 @@
 export { handleRequest } from "./core/proxy-handler";
 export type { DecartProxyOptions } from "./core/types";
-export { handler as decartProxy } from "./express/middleware";
-export {
-  default as decartProxyNextjs,
-  handlerAppRouter as decartProxyAppRouter,
-  handlerPagesRouter as decartProxyPagesRouter,
-  PROXY_ROUTE as nextjsRoute,
-  route,
-} from "./nextjs/route";
